84b1568d754c3bdef9b2d3509d196c7e89b7903c
kevinjmosley93 <kevinjmosley93@gmail.com>
2020-11-07 19:12:13 -0600

    Latest commit

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const chalk = require("chalk");
 
 // require route files
 const exampleRoutes = require("./app/routes/example_routes");
